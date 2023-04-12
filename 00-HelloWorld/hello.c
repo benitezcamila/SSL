570a524c7f6550b5a47f15e766e6d6cd78ab0e93
Camila Benitez <cabenitez@frba.utn.edu.ar>
2023-04-12 07:49:59 -0300

    cambio carpeta

--- 00-HelloWorld/hello.c
+++ 00-HelloWorld/hello.c
@@ -1,1 +1,13 @@
-aca codigo
+#include <stdio.h>
+
+void crearArchivo() {
+    FILE *archivo;
+    archivo = fopen("output.txt", "w");
+    fprintf(archivo, "Hello world!");
+    fclose(archivo);
+}
+
+int main() {
+    crearArchivo();
+    return 0;
+}
